0ca63f2abcc95db73fb3cc50b0f1e3fe9e9b53c2
MAnuragnaidu <anurag.madhamshetti@gmail.com>
2026-04-23 01:13:42 +0530

    update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,9 @@
-process.env['DATABASE_URL'] = 'file:./dev.db';
 import { PrismaClient } from '@prisma/client';
 import { PrismaLibSql } from '@prisma/adapter-libsql';
-import { createClient } from '@libsql/client';
 
-const dbUrl = 'file:./dev.db';
-
-const libsql = createClient({
-  url: dbUrl
+const adapter = new PrismaLibSql({
+  url: 'file:./dev.db',
 });
-
-const adapter = new PrismaLibSql(libsql);
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
