f7362d34e0e3cc20c6b9b29b1ba2d61a3db8cf91
MAnuragnaidu <anurag.madhamshetti@gmail.com>
2026-04-23 01:16:59 +0530

    updated

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaLibSql } from '@prisma/adapter-libsql';
-
-const adapter = new PrismaLibSql({
-  url: 'file:./dev.db',
-});
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-    log: ['query'],
-  });
+  new PrismaClient({ log: ['query'] });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
